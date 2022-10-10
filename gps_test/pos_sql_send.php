480e8dc42ba080b3c09bf1a6dce6058b17967d19
NicholasEiti <nicholaseiti@gmail.com>
2022-10-10 22:13:08 +0000

    Adicionamento de Comentários

--- gps_test/pos_sql_send.php
+++ gps_test/pos_sql_send.php
@@ -6,7 +6,6 @@ Código em PHP que irá capturar as informações de uma tabela de SQL e imprimi
 Localização ideal dos arquivos (Labrador): /var/www/html/api
 
 Fonte: Random Nerd Tutorial (https://randomnerdtutorials.com/esp32-esp8266-raspberry-pi-lamp-server/)
-Autor: Nicholas Eiti Dan
 */
 
 echo "<p>Esta página enviará os dados do coordenadas para uma tabela em 
@@ -21,19 +20,22 @@ $lat = $lng = $deglat = $deglng = "";
 
 # Verificar se chegou informações vindo de outro cliente HTTP (utilizando o método POST)
 if ($_SERVER['REQUEST_METHOD'] == "POST"){
+	# Tratamento dos dados dentro de $_POST
 	$lat = htmlspecialchars($_POST['latitude']);
 	$lng = htmlspecialchars($_POST['longitude']);
 
 	# Cria conexão com a tabela SQL
 	$conn = new mysqli($servername, $username, $passwd, $dbname);
 
+	# Verifica a integridade da conexão
 	if ($conn->connect_error){
 		die("Connection failed: " . $conn->connect_error);
 	}
 
-	# Sentença de SQL
+	# Sentença de SQL a ser processada
 	$sql = "INSERT INTO gps_info (`lat`,`long`) VALUES ('" . $lat . "', '" . $lng . "')";
 
+	# Verifica se o processo da sentença foi feita com sucesso
 	if ($conn->query($sql) === TRUE){
 		echo "Nova linha inserida com sucesso!";
 	}
